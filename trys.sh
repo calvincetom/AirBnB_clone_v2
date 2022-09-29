00f87970f24a083810dfa9aa4cea00e0d63f1600
calvincetom <calvincetom@gmail.com>
2022-09-29 14:26:36 +0300

    refactor deployment

---
+++ trys.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#try commmand **
+sudo fab -f 2-do_deploy_web_static.py do_deploy:archive_path=versions/web_static_20220929112331.tgz -i ~/home/calvince/.ssh/id_rsa -u ubuntu
+sudo fab -f 2-do_deploy_web_static.py do_deploy:archive_path=versions/web_static_20220929112331.tgz -i ~/home/calvince/.ssh/id_rsa -u ubuntu
+sudo fab -f 2-do_deploy_web_static.py do_deploy:archive_path=versions/web_static_20220929112331.tgz -i ~/home/calvince/.ssh/id_rsa -u ubuntu
+sudo fab -f 2-do_deploy_web_static.py do_deploy:archive_path=versions/web_static_20220929112331.tgz -i ~/home/calvince/.ssh/id_rsa -u ubuntu
+sudo fab -f 2-do_deploy_web_static.py do_deploy:archive_path=versions/web_static_20220929112331.tgz -i ~/home/calvince/.ssh/id_rsa -u ubuntu
+sudo fab -f 2-do_deploy_web_static.py do_deploy:archive_path=versions/web_static_20220929112331.tgz -i ~/home/calvince/.ssh/id_rsa -u ubuntu
+
